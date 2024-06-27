39632293598c18d08ec6cf6858b37d84a01ee7ed
danhnguyen <danhnguyen@beeknights.com>
2024-06-27 11:47:54 +0700

    fix: build

--- examples/src/views/Home.vue
+++ examples/src/views/Home.vue
@@ -1,3 +1,59 @@
+<script lang="ts" setup>
+import { computed, watchEffect } from 'vue';
+import { GeolocateControl, Mapbox } from '@libs/components';
+import { useFitBounds, useMapbox } from '@libs/composables';
+import type { LngLatBoundsLike, MapOptions } from 'vue3-mapbox';
+import circle from '@turf/circle';
+import bbox from '@turf/bbox';
+import 'vue3-mapbox/dist/style.css';
+
+const options = computed<MapOptions>(() => ({
+  container: 'map',
+  style: 'https://worldwidemaps.sqkii.com/api/maps/purple/style.json',
+  center: [103.8198, 1.3521],
+  zoom: 12,
+  minZoom: 9,
+  maxZoom: 20,
+}));
+
+const { register: registerMap, mapInstance } = useMapbox();
+
+const { setFitBounds } = useFitBounds(mapInstance);
+
+watchEffect(() => {
+  console.log('mapInstance.value', mapInstance.value);
+});
+
+async function goToBounds() {
+  const _circle = circle([106.69390849397668, 10.789432788366994], 356 * 1.2, {
+    units: 'meters',
+  });
+  const box = bbox(_circle);
+
+  console.log('box', box);
+
+  await setFitBounds(box as LngLatBoundsLike, {
+    duration: 2000,
+    animate: true,
+  });
+}
+</script>
 <template>
-  <div>Home</div>
+  <Mapbox :options="options" @register="registerMap">
+    <button
+      style="position: absolute; top: 0; left: 0; z-index: 10"
+      @click="goToBounds"
+    >
+      fit bounds
+    </button>
+    <GeolocateControl
+      :options="{
+        positionOptions: {
+          enableHighAccuracy: true,
+        },
+        trackUserLocation: true,
+        showAccuracyCircle: false,
+      }"
+    />
+  </Mapbox>
 </template>
--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,11 @@
 import { defineConfig } from 'vite';
 import { resolve } from 'path';
 import vue from '@vitejs/plugin-vue';
+import dts from 'vite-plugin-dts';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [vue(), dts()],
   resolve: {
     alias: {
       '@': resolve(__dirname, './src'),
